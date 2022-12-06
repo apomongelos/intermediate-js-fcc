491e7909813b1c0313c8f84b35c300aab708ed71
Fernando Mongelos <fernandomongelos911@gmail.com>
2022-12-05 21:51:57 -0300

    template literals,object literal declarations and declarative funtions

---
+++ ES6/exercise16.js
@@ -0,0 +1,14 @@
+/**
+ * Exercise Sixteen
+ * Write Concise Object Literal Declarations Using Object Property Shorthand
+ */
+
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender,
+  };
+  // Only change code above this line
+};
