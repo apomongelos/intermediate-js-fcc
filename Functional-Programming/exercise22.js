2ef1d14ece0a4dc47669ed8c31b3586672beb4f2
Fernando Mongelos <fernandomongelos911@gmail.com>
2022-12-05 22:11:52 -0300

    every and some methods

---
+++ Functional-Programming/exercise22.js
@@ -0,0 +1,12 @@
+/**
+ * Exercise Twenty-Two
+ * Use the every Method to Check that Every Element in an Array Meets a Criteria
+ */
+
+function checkPositive(arr) {
+  // Only change code below this line
+  return arr.every((num) => num > 0);
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
---
+++ Functional-Programming/exercise23.js
@@ -0,0 +1,12 @@
+/**
+ * Exercise Twenty-Three
+ * Use the some Method to Check that Any Elements in an Array Meet a Criteria
+ */
+
+function checkPositive(arr) {
+  // Only change code below this line
+  return arr.some((num) => num > 0);
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
