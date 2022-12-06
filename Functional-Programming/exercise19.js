d5aee94f4c9b11a1b39148abd82f18a3fae44217
Fernando Mongelos <fernandomongelos911@gmail.com>
2022-12-05 22:10:33 -0300

    string methods combined with arrays methods

---
+++ Functional-Programming/exercise19.js
@@ -0,0 +1,12 @@
+/**
+ * Exercise Nineteen
+ * Split a String into an Array Using the split Method
+ */
+
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/\W/);
+  // Only change code above this line
+}
+
+splitify('Hello World,I-am code');
